4c18644c15a79c512c420ee769a94e1b36fe5c8d
billyevans <pervushinai@gmail.com>
2014-09-10 22:30:34 +0400

    Add sublime-links

--- mklinks.sh
+++ mklinks.sh
@@ -7,5 +7,14 @@ do
 	ln -s $DIR/$f ~/.$f
 done
 
+if [ `uname` == "Darwin" ]
+then
+	for f in Preferences.sublime-settings CPP.sublime-build
+	do
+		ln -s $DIR/$f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/$f
+	done
+fi
+
+
 mkdir -p ~/.ssh/
 ln -s $DIR/ssh/config ~/.ssh/config
