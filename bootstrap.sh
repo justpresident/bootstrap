964edeae1e740b2a112b8b0bd97f8930eddadbfa
Roman Studenikin <justpresident@gmail.com>
2015-06-15 13:56:48 +0300

    config fonts

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,12 +10,17 @@ if [[ `grep $DIR/.bashrc ~/.bashrc` == '' ]]; then
 	echo fi                             >> ~/.bashrc
 fi
 
-for f in .gitconfig .inputrc .profile .vimrc .vim .my.cnf .tmux.conf
+for f in .gitconfig .inputrc .profile .vimrc .vim .my.cnf .tmux.conf .fonts
 do
 	rm -rf ~/$f
 	ln -vs $DIR/$f ~/$f
 done
 
+read -n 1 -p "Would you like to update fonts? (y/N) " UPDATE_FONTS
+if [[ -n $UPDATE_FONTS && $UPDATE_FONTS == 'y' ]]; then
+	echo "updating fonts";
+	sudo fc-cache -f -v
+fi
 
 mkdir -p ~/.ssh/
 ln -vfs $DIR/.ssh/config ~/.ssh/config
