302021033f722d6c4a63f17d7393bad35f2d46c7
Roman Studenikin <roman.studenikin@booking.com>
2015-09-05 13:31:51 +0200

    clean one more bin

--- bin/disable_indexer.sh
+++ bin/disable_indexer.sh
@@ -1,9 +1,16 @@
-sudo mv /usr/bin/baloo_file /usr/bin/baloo_file.orig
-sudo ln -s /bin/true /usr/bin/baloo_file
-sudo mv /usr/bin/baloo_file_cleaner /usr/bin/baloo_file_cleaner.orig
-sudo ln -s /bin/true /usr/bin/baloo_file_cleaner
-sudo mv /usr/bin/baloo_file_extractor /usr/bin/baloo_file_extractor.orig
-sudo ln -s /bin/true /usr/bin/baloo_file_extractor
+set -e
+
+FAKE=/bin/true
+
+for f in baloo_file baloo_file_cleaner baloo_file_extractor akonadi_baloo_indexer; do
+    if [[ $(readlink -f /usr/bin/${f}) != $FAKE ]]; then
+        sudo mv /usr/bin/${f} "${f}.orig"
+        sudo ln -s ${FAKE} /usr/bin/$f
+        sudo killall $f 
+    else
+        ls -la /usr/bin/${f}
+    fi
+done
 
 echo Indexing-Enabled=false >> $HOME/.kde/share/config/baloofilerc
 ps ax | grep baloo
