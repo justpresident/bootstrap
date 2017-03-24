e5e3012d7d7da266bf9bfd5498ffcbf02f503602
Roman Studenikin <roman.studenikin@booking.com>
2017-03-24 22:39:45 +0100

    lock screen after wakeup from suspend

--- install_deps.sh
+++ install_deps.sh
@@ -3,3 +3,6 @@
 apt-get install \
     git vim yakuake \
     fluxbox i3lock gnome-screenshot acpi notify-osd
+
+user=$(whoami)
+echo -e "\n\nPlease execute visudo and add following row at the very bottom:\n $user ALL=(ALL) NOPASSWD: /usr/sbin/pm-suspend, /sbin/reboot, /sbin/halt\n"
