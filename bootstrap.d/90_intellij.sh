f03fdd690a297744c0d489f3bed1a920e93524af
Roman Studenikin <roman.studenikin@booking.com>
2019-09-01 21:20:32 +0200

    check for empty ide path

--- bootstrap.d/90_intellij.sh
+++ bootstrap.d/90_intellij.sh
@@ -1,5 +1,7 @@
 IJ_CONF_PATH=$(ls -a1 $HOME | grep -e ^.Idea | tail -n1)
 
-echo "Installing IntelliJ Idea configs into $IJ_CONF_PATH"
 
-cp -rv ij_idea/* $HOME/$IJ_CONF_PATH/
+if [[ -n $IJ_CONF_PATH ]]; then
+    echo "Installing IntelliJ Idea configs into $IJ_CONF_PATH"
+    cp -rv ij_idea/* $HOME/$IJ_CONF_PATH/
+fi
