673fa729c1168916a71ccf0d18566b38f35ee8f7
Roman Studenikin <srv@meta.com>
2025-07-10 22:26:56 +0100

    Add telescope-cache config

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -358,6 +358,30 @@ require('lazy').setup({
   -- you do for a plugin at the top level, you can do for a dependency.
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
+    {
+        'justpresident/telescope-cache',
+        config = function()
+            require('telescope').setup {
+                extensions = {
+                    ['cache'] = {
+                        cache_dir = vim.fn.stdpath('cache') .. '/telescope-cache',
+                        directories = {
+                            vim.fn.expand('$HOME/github/telescope-cache'),
+                        },
+                        allow_patterns = {
+                            '%.lua$', '%.py$', '%.rs$', '%.c$', "%.h$", "%.cpp$", '%.thrift$',
+                            "Makefile$", "Dockerfile$", "BUCK$", "TARGETS$", "%.bzl$"
+                        },
+                        ignore_patterns = { '.git', '__pycache__', '.pytest_cache', 'target', 'build', 'buck-out' },
+                        max_file_size = 1024 * 1024,
+                        auto_refresh = true,
+                        refresh_interval = 3600,
+                        use_encryption = true,
+                    }
+                }
+            }
+        end,
+    },
 
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
@@ -365,6 +389,7 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'justpresident/telescope-cache',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
 
@@ -425,6 +450,8 @@ require('lazy').setup({
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
+          ['cache'] = {
+          }
         },
       }
 
@@ -443,6 +470,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      require('telescope').load_extension('cache')
+
+      vim.keymap.set('n', '<leader>scf', '<cmd>TelescopeCacheFiles<cr>', { desc = '[S]earch [C]ached [F]iles' })
+      vim.keymap.set('n', '<leader>scg', '<cmd>TelescopeCacheGrep<cr>', { desc = '[S]earch [C]ached by [G]rep' })
+      vim.keymap.set('n', '<leader>scr', '<cmd>TelescopeCacheRefresh<cr>', { desc = '[S]earch [C]ache [R]efresh' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -873,7 +906,7 @@ require('lazy').setup({
   },
   dev = {
     path = "~/github",
-    patterns = { "srv-meta" },
+    patterns = { "srv-meta", "justpresident" },
     fallback = true,
   },
   install = {
